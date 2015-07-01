fc69d8e93087201b9ca1eeb16ee0485784c534a1
ken.watanabe <ken.watanabe@gree.co.jp>
2015-07-01 13:32:14 -0700

    Add Lobby class

---
+++ Classes/Lobby.cpp
@@ -0,0 +1,25 @@
+//
+//  Lobby.cpp
+//  Tetrominos
+//
+//  Created by Ken Watanabe on 2015/07/01.
+//
+//
+
+#include "Lobby.h"
+
+using namespace cocos2d;
+
+bool Lobby::init()
+{
+    if (! Node::init())
+    {
+        return false;
+    }
+    
+    LayerColor* background = LayerColor::create(Color4B(255, 255, 255, 255));
+    
+    this->addChild(background);
+    
+    return true;
+}
